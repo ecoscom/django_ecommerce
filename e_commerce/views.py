4f0477911d3bb687cf5891a6c17d3f3fdcf7fcd6
Evandro da Costa <evandrodacosta@gmail.com>
2021-05-29 19:21:55 -0300

    Add views about_page and contact_page, add urls of views

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 def home_page(request):
-    return render(request, 'home_page.html', {})
+    context = {
+        'title': 'Página principal',
+        'content': 'Bem-vindo!'
+    }
+    return render(request, 'home_page.html', context)
+
+def about_page(request):
+    context = {
+        'title': 'Página sobre',
+        'content': 'Bem-vindo a página Sobre!'
+    }
+    return render(request, 'about/view.html', context)
+
+def contact_page(request):
+    context = {
+        'title': 'Página de contato',
+        'content': 'Bem-vindo a página de contato!'
+    }
+    return render(request, 'contact/view.html', context)
