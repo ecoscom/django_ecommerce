88635f1fa613350ad678ab04be74d4f3ab31f7eb
Evandro da Costa <evandrodacosta@gmail.com>
2021-06-03 18:07:23 -0300

    Update views.py (add method login_page and register_page)

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import render, redirect
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
     context = {
         'title': 'Página principal',
         'content': 'Bem-vindo!'
     }
+    if request.user.is_authenticate:
+        context['premium_content'] = 'Você é um usuário Premium'
     return render(request, 'home_page.html', context)
 
 def about_page(request):
@@ -27,3 +30,37 @@ def contact_page(request):
         if contact_form.is_valid():
             print(contact_form.cleaned_data)
     return render(request, 'contact/view.html', context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+    print('User logged in')
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'auth/login.html', context)
+
+User = get_user_model()
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, 'auth/register.html', context)
