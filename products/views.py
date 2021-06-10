caf3e472cf7914c68ca452c3ab7de6d129c4a3fb
Evandro da Costa <evandrodacosta@gmail.com>
2021-06-09 22:45:44 -0300

    update products/views.py and products/list.html

--- products/views.py
+++ products/views.py
@@ -6,3 +6,6 @@ from .models import Product
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'products/list.html'
+
+    def get_context_data(self, *args, **kwargs):
+        return super(ProductListView, self).get_context_data(*args, **kwargs)
