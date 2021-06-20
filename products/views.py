f817b3c12d5d3de8ea26ec9451dbbb5e45e5fc7c
Evandro da Costa <evandrodacosta@gmail.com>
2021-06-20 17:09:13 -0300

    update products/view.py and create products/templates/products/detail.html

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from .models import Product
@@ -13,6 +13,12 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = 'products/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super.get_context_data(self, **kwargs)
+        return context
+        
+         
 
 def product_detail_view(request):
     pass
